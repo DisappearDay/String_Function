1cbb6962ecd9f79b41250f9b04592f8e536ae038
DisappearDay <117563652+DisappearDay@users.noreply.github.com>
2023-04-09 13:16:14 +0800

    Update test.c

--- test.c
+++ test.c
@@ -207,3 +207,109 @@ int main() {
 	return 0;
 }
 
+//int main() {
+//	char arr[] = "hello.world!and";
+//	char* p = ".!";
+//	//char arr2[] = "2981062773@qq@com";
+//	//char* p2 = "@.";
+//
+//	//hello.world!and
+//	char buf[1024] = { 0 };
+//	strcpy(buf, arr);
+//	char* ret = NULL;
+//	for (ret = strtok(arr, p); ret != NULL;ret=strtok(NULL,p)) {
+//		printf("%s\n",ret);
+//	}
+//
+//	//char* ret=strtok(arr, p);
+//	//printf("%s\n", ret);
+//
+//	////char* ret1 = strtok(arr2, p2);
+//	////printf("%s\n", ret1);
+//
+//	//ret=strtok(NULL, p);
+//	//printf("%s\n",ret);
+//
+//	//ret=strtok(NULL, p);
+//	//printf("%s\n", ret);
+//
+//	return 0;
+//}
+
+//
+//int main() {
+//	//错误码   错误信息
+//	//0 -      No error
+//	//1 -      Operation not permitted
+//	//2 -      No such file or directory
+//	//...      ...
+//
+//	//全局的错误变量/错误码的变量
+//	//当c语言的库函数在执行过程中，发生了错误，就会把对应的错误码赋值到errno中
+//	/*char* arr = strerror(errno);
+//	printf("%s\n", arr);*/
+//
+//	FILE* pf=fopen("text.txt","r");
+//	if (pf==NULL) 
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		printf("open file success");
+//	}
+//
+//	return 0;
+//
+//}
+
+
+int main() {
+
+	//char ch = 'w';
+	////如果是小写字母返回非零值，如果不是小写字符返回0；
+	//int ret = islower(ch);
+	////十进制数字0-9
+	//int ret1 = isdigit(ch);
+	////空白字符“空格” 换页"\f" 换行"\n"  回车"\r" 制表符"\t" 垂直制表符"\v"
+	//int ret1 = isspace(ch);
+	////十六进制包括a-f A-F
+	//int ret1 = isxdigit(ch);
+	////任何控制字符
+	//int ret1 = iscntrl(ch);
+	////大写字母A-Z
+	//int ret1 = isupper(ch);
+	////字母a-z，字母A-Z
+	//int ret1 = isalpha(ch);
+	////字母或者数字 a-z，字母A-Z  数字0-9
+	//int ret1 = isalnum(ch);
+	////标点符号，任何不属于数字或者字母的图形字符
+	//int ret1 = ispunct(ch);
+	////任何图形字符
+	//int ret1 = isgraph(ch);
+	////任何可打印字符包括图形字符和空白字符
+	//int ret1 = isprint(ch);
+	//int ret1 = printf("%d\n", ret);
+
+	////字符转换
+	////tolower--转小写字母
+	////toupper--转大写字母
+	//
+	////如果是小写不动，大写转化为小写
+	//char ch = tolower('Q');
+	////printf("%c\n",ch);
+	//putchar(ch);
+
+	char arr[] = "I Am A Studen";
+	int i = 0;
+	while (arr[i]) {
+		if (isupper(arr[i])) {
+			arr[i] = tolower(arr[i]);
+		}
+		i++;
+	}
+	printf("%s\n", arr);
+
+
+	return 0;
+}
